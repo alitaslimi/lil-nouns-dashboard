db53df35c108f729c063bf4c1896a4a6e88a48e9
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-01-18 05:03:52 +0330

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ with open('style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 # Data Sources
-@st.cache(ttl=3600)
+# @st.cache(ttl=3600)
 def get_data(query):
     if query == 'Mints Overview':
         return pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/c88b2d6a-158b-4b1a-bc18-0a3920ada097/data/latest')
